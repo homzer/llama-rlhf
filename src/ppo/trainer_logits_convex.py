0b543a68240d573bb8c6c36be5a90f201f56fa80
homzer <homzer@qq.com>
2025-11-27 11:44:53 +0800

    lco beta

--- src/ppo/trainer_logits_convex.py
+++ src/ppo/trainer_logits_convex.py
@@ -14,14 +14,12 @@ class ParallelLCOTrainerForCausalLM(ParallelTrainer):
             policy: ParallelModelForCausalLM,
             optimizer: torch.optim.Optimizer,
             beta: float = 10.0,
-            estimate_with_beta: bool = False,
             save_optim: bool = False,
             accumulation_steps: int = 1,
     ):
         super().__init__(policy, optimizer, save_optim=save_optim, accumulation_steps=accumulation_steps)
         self.policy = policy
         self.beta = beta
-        self.estimate_with_beta = estimate_with_beta
         self.criterion = torch.nn.KLDivLoss(reduction="none", log_target=True)
 
     def forward(self, rollout_data):
@@ -36,17 +34,40 @@ class ParallelLCOTrainerForCausalLM(ParallelTrainer):
         actions = actions.view(-1)[action_masks.view(-1)]
         advantages = advantages.view(-1)[action_masks.view(-1)]
         old_logits = old_logits.view(-1, old_logits.shape[-1])[action_masks.view(-1)]
+        pos_advantage_masks = advantages > 0
+        neg_advantage_masks = ~ pos_advantage_masks
 
         logits = self.policy.forward(obs).logits
         logits = logits.view(-1, logits.shape[-1])[action_masks.view(-1)]
 
         advantages = estimate_vocab_adv(old_logits, advantages.unsqueeze(-1), actions.unsqueeze(-1))
-        log_targets = create_lco_log_target(old_logits, advantages, beta=self.beta)
-        loss = self.criterion.forward(
-            torch.log_softmax(logits, dim=-1), target=log_targets.to(logits)
-        ).sum(-1).mean()
 
+        # compute loss for positive advantage tokens
+        loss_pos = torch.tensor(0.0).to(logits)
+        if torch.sum(pos_advantage_masks).item() != 0:
+            pos_log_targets = create_lco_log_target(
+                old_logits[pos_advantage_masks], advantages[pos_advantage_masks], beta=1.0
+            )
+            loss_pos = self.criterion.forward(
+                torch.log_softmax(logits[pos_advantage_masks], dim=-1), target=pos_log_targets.to(logits)
+            ).sum(-1).mean()
+
+        # compute loss for negative advantage tokens
+        loss_neg = torch.tensor(0.0).to(logits)
+        if torch.sum(neg_advantage_masks).item() != 0:
+            neg_log_targets = create_lco_log_target(
+                old_logits[neg_advantage_masks], advantages[neg_advantage_masks], beta=self.beta
+            )
+            loss_neg = self.criterion.forward(
+                torch.log_softmax(logits[neg_advantage_masks], dim=-1), target=neg_log_targets.to(logits)
+            ).sum(-1).mean()
+
+        loss = loss_pos + loss_neg
         self.backward(loss)
+
+        if (self.step + 1) % 100 == 0:
+            print(f"Positive Reward Loss: {loss_pos.item()} | Negative Reward Loss: {loss_neg.item()}")
+
         Outputs = collections.namedtuple('Outputs', ['loss'])
         return Outputs(loss=loss.item())
 
