cc2d290894e8b56381c8d048c9859e65c3c59716
homzer <homzer@qq.com>
2025-12-05 16:59:23 +0800

    rotary embedding

--- src/models/modeling_acts.py
+++ src/models/modeling_acts.py
@@ -155,36 +155,36 @@ class LogitsNormalize:
 
 
 # Copied from Huggingface
-# class RotaryEmbedding(nn.Module):
-#     def __init__(self, dim, max_position_embeddings=2048, base=10000, device=None):
-#         super().__init__()
-#         inv_freq = 1.0 / (base ** (torch.arange(0, dim, 2).type(torch.float32).to(device) / dim))
-#         self.register_buffer("inv_freq", inv_freq)
-#
-#         # Build here to make `torch.jit.trace` work.
-#         self.max_seq_len_cached = max_position_embeddings
-#         t = torch.arange(self.max_seq_len_cached, device=self.inv_freq.device, dtype=self.inv_freq.dtype)
-#         freqs = torch.einsum("i,j->ij", t, self.inv_freq)
-#         # Different from paper, but it uses a different permutation in order to obtain the same calculation
-#         emb = torch.cat((freqs, freqs), dim=-1)
-#         self.register_buffer("cos_cached", emb.cos()[None, None, :, :], persistent=False)
-#         self.register_buffer("sin_cached", emb.sin()[None, None, :, :], persistent=False)
-#
-#     def forward(self, x, seq_len=None):
-#         # x: [bs, num_attention_heads, seq_len, head_size]
-#         # This `if` block is unlikely to be run after we build sin/cos in `__init__`. Keep the logic here just in case.
-#         if seq_len > self.max_seq_len_cached:
-#             self.max_seq_len_cached = seq_len
-#             t = torch.arange(self.max_seq_len_cached, device=x.device, dtype=self.inv_freq.dtype)
-#             freqs = torch.einsum("i,j->ij", t, self.inv_freq)
-#             # Different from paper, but it uses a different permutation in order to obtain the same calculation
-#             emb = torch.cat((freqs, freqs), dim=-1).to(x.device)
-#             self.register_buffer("cos_cached", emb.cos()[None, None, :, :], persistent=False)
-#             self.register_buffer("sin_cached", emb.sin()[None, None, :, :], persistent=False)
-#         return (
-#             self.cos_cached[:, :, :seq_len, ...].to(dtype=x.dtype),  # [1, 1, seq_len, head_dim]
-#             self.sin_cached[:, :, :seq_len, ...].to(dtype=x.dtype),  # [1, 1, seq_len, head_dim]
-#         )
+class RotaryEmbedding_(nn.Module):
+    def __init__(self, dim, max_position_embeddings=2048, base=10000, device=None):
+        super().__init__()
+        inv_freq = 1.0 / (base ** (torch.arange(0, dim, 2).type(torch.float32).to(device) / dim))
+        self.register_buffer("inv_freq", inv_freq)
+
+        # Build here to make `torch.jit.trace` work.
+        self.max_seq_len_cached = max_position_embeddings
+        t = torch.arange(self.max_seq_len_cached, device=self.inv_freq.device, dtype=self.inv_freq.dtype)
+        freqs = torch.einsum("i,j->ij", t, self.inv_freq)
+        # Different from paper, but it uses a different permutation in order to obtain the same calculation
+        emb = torch.cat((freqs, freqs), dim=-1)
+        self.register_buffer("cos_cached", emb.cos()[None, None, :, :], persistent=False)
+        self.register_buffer("sin_cached", emb.sin()[None, None, :, :], persistent=False)
+
+    def forward(self, x, seq_len=None):
+        # x: [bs, num_attention_heads, seq_len, head_size]
+        # This `if` block is unlikely to be run after we build sin/cos in `__init__`. Keep the logic here just in case.
+        if seq_len > self.max_seq_len_cached:
+            self.max_seq_len_cached = seq_len
+            t = torch.arange(self.max_seq_len_cached, device=x.device, dtype=self.inv_freq.dtype)
+            freqs = torch.einsum("i,j->ij", t, self.inv_freq)
+            # Different from paper, but it uses a different permutation in order to obtain the same calculation
+            emb = torch.cat((freqs, freqs), dim=-1).to(x.device)
+            self.register_buffer("cos_cached", emb.cos()[None, None, :, :], persistent=False)
+            self.register_buffer("sin_cached", emb.sin()[None, None, :, :], persistent=False)
+        return (
+            self.cos_cached[:, :, :seq_len, ...].to(dtype=x.dtype),  # [1, 1, seq_len, head_dim]
+            self.sin_cached[:, :, :seq_len, ...].to(dtype=x.dtype),  # [1, 1, seq_len, head_dim]
+        )
 
 
 class RotaryEmbedding(nn.Module):
@@ -196,7 +196,7 @@ class RotaryEmbedding(nn.Module):
 
     @torch.no_grad()
     def forward(self, x, seq_len=None):
-        t = torch.arange(seq_len).to(x)
+        t = torch.arange(seq_len, device=self.inv_freq.device, dtype=self.inv_freq.dtype)
         freqs = torch.einsum("i,j->ij", t, self.inv_freq)
         emb = torch.cat((freqs, freqs), dim=-1)
-        return emb.cos()[None, None, :, :], emb.sin()[None, None, :, :]
+        return emb.cos()[None, None, :, :].to(x.dtype), emb.sin()[None, None, :, :].to(x.dtype)
