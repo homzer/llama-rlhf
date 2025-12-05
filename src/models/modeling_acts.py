c56021a8afd7dbef3657724f081bf351e85b082b
homzer <homzer@qq.com>
2025-12-05 17:25:29 +0800

    rotary embedding

--- src/models/modeling_acts.py
+++ src/models/modeling_acts.py
@@ -155,7 +155,7 @@ class LogitsNormalize:
 
 
 # Copied from Huggingface
-class RotaryEmbedding(nn.Module):
+class RotaryEmbedding_(nn.Module):
     def __init__(self, dim, max_position_embeddings=2048, base=10000, device=None):
         super().__init__()
         inv_freq = 1.0 / (base ** (torch.arange(0, dim, 2).type(torch.float32).to(device) / dim))
@@ -184,6 +184,38 @@ class RotaryEmbedding(nn.Module):
         return (
             self.cos_cached[:, :, :seq_len, ...].to(dtype=x.dtype),  # [1, 1, seq_len, head_dim]
             self.sin_cached[:, :, :seq_len, ...].to(dtype=x.dtype),  # [1, 1, seq_len, head_dim]
+        )
+
+
+class RotaryEmbedding(nn.Module):
+    def __init__(self, dim, max_position_embeddings=2048, base=10000, device=None):
+        super().__init__()
+        inv_freq = 1.0 / (base ** (torch.arange(0, dim, 2).type(torch.float32).to(device) / dim))
+        self.register_buffer("inv_freq", inv_freq)
+
+        # Build here to make `torch.jit.trace` work.
+        self.max_seq_len_cached = max_position_embeddings
+        # t = torch.arange(self.max_seq_len_cached, device=self.inv_freq.device, dtype=self.inv_freq.dtype)
+        # freqs = torch.einsum("i,j->ij", t, self.inv_freq)
+        # # Different from paper, but it uses a different permutation in order to obtain the same calculation
+        # emb = torch.cat((freqs, freqs), dim=-1)
+        # self.register_buffer("cos_cached", emb.cos()[None, None, :, :], persistent=False)
+        # self.register_buffer("sin_cached", emb.sin()[None, None, :, :], persistent=False)
+
+    def forward(self, x, seq_len=None):
+        self.max_seq_len_cached = seq_len
+        t = torch.arange(self.max_seq_len_cached, device=x.device, dtype=self.inv_freq.dtype)
+        freqs = torch.einsum("i,j->ij", t, self.inv_freq)
+        # Different from paper, but it uses a different permutation in order to obtain the same calculation
+        emb = torch.cat((freqs, freqs), dim=-1).to(x.device)
+        cos_cached = emb.cos()[None, None, :, :]
+        sin_cached = emb.sin()[None, None, :, :]
+        # self.register_buffer("cos_cached", emb.cos()[None, None, :, :], persistent=False)
+        # self.register_buffer("sin_cached", emb.sin()[None, None, :, :], persistent=False)
+
+        return (
+            cos_cached[:, :, :seq_len, ...].to(dtype=x.dtype),  # [1, 1, seq_len, head_dim]
+            sin_cached[:, :, :seq_len, ...].to(dtype=x.dtype),  # [1, 1, seq_len, head_dim]
         )
 
 
