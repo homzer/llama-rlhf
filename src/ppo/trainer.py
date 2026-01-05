9a9b557cf860a4a0f512177e37af81f9e42d0979
homzer <homzer@qq.com>
2026-01-05 13:52:41 +0800

    minillm

--- src/ppo/trainer.py
+++ src/ppo/trainer.py
@@ -442,16 +442,16 @@ class ParallelMiniLLMTrainerForCausalLM(ParallelTrainer):
         mix_action_logprobs = torch.log(
             self.alpha * teacher_action_logprobs.exp() + (1 - self.alpha) * old_action_logprobs.exp()
         )
-        importance_weights = torch.cumsum(old_action_logprobs - mix_action_logprobs, dim=-1).exp()
         normed_rewards = torch.cumsum((teacher_action_logprobs - old_action_logprobs).flip(-1), dim=-1).flip(-1)
+        normed_rewards = normed_rewards / torch.cumsum(action_masks.filp(-1), dim=-1).flip(-1)
 
         logits = self.policy.forward(obs).logits
         logits = logits.view(-1, logits.shape[-1])[action_masks.view(-1)]
         teacher_logits = teacher_logits.view(-1, teacher_logits.shape[-1])[action_masks.view(-1)]
         actions = actions.view(-1)[action_masks.view(-1)]
         mix_action_logprobs = mix_action_logprobs.view(-1)[action_masks.view(-1)]
-        importance_weights = importance_weights.view(-1)[action_masks.view(-1)]
         normed_rewards = normed_rewards.view(-1)[action_masks.view(-1)]
+        importance_weights = (old_action_logprobs - mix_action_logprobs).exp()
 
         action_logprobs = torch.gather(
             torch.log_softmax(logits, dim=-1), dim=-1, index=actions.unsqueeze(-1)
