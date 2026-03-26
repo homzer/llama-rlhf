433a4c70155546d8be55798713f82ce3086aa46c
homzer <homzer@qq.com>
2026-03-26 17:39:23 +0800

    kimi

---
+++ policy_train_kimi_with_rule_rm.py
@@ -0,0 +1,249 @@
+""" Mirror Descent Method from Kimi K1.5 """
+import collections
+import gc
+import os
+import shutil
+
+import fire
+import torch
+
+from policy_train_grpo import compute_grpo_rewards
+from policy_train_ppo_with_evaluate import evaluate_actor
+from policy_train_ppo_with_rule_rm import collect_actor_buffer_with_label, collect_rule_based_verifier_buffer
+from src.dataset import JsonDataset
+from src.entities import IterationHandler, Timer
+from src.models.modeling import AutoModelForCausalLM
+from src.models.modeling import ParallelModelForCausalLM
+from src.parallel.initialize import setup_model_parallel, set_barrier, get_rank
+from src.ppo.buffer import RolloutBuffer
+from src.trainer import ParallelTrainer
+from src.utils import json_load, print_current_func_args
+
+
+class ParallelKimiTrainerForCausalLM(ParallelTrainer):
+    def __init__(
+            self,
+            policy: ParallelModelForCausalLM,
+            optimizer: torch.optim.Optimizer,
+            beta: float = None,
+            save_optim: bool = False,
+            accumulation_steps: int = 1
+    ):
+        super().__init__(policy, optimizer, save_optim, accumulation_steps=accumulation_steps)
+        self.policy = policy
+        self.beta = beta or 1.0
+
+    def forward(self, rollout_data):
+        self.policy.train()
+
+        obs = rollout_data.obs.to(self.policy.device())
+        actions = rollout_data.actions.to(self.policy.device())
+        action_masks = rollout_data.action_masks.to(self.policy.device())
+        advantages = rollout_data.advantages.to(self.policy.device())
+        old_action_logprobs = rollout_data.action_logprobs.to(self.policy.device())
+
+        actions = torch.masked_select(actions.view(-1), action_masks.view(-1))
+        advantages = torch.masked_select(advantages.view(-1), action_masks.view(-1))
+        old_action_logprobs = torch.masked_select(old_action_logprobs.view(-1), action_masks.view(-1))
+
+        logits = self.policy.forward(obs).logits
+        logits = logits.view(-1, logits.shape[-1])[action_masks.view(-1)]
+        advantages = advantages.to(logits.dtype)
+        action_logprobs = torch.gather(
+            torch.log_softmax(logits, dim=-1), dim=-1, index=actions.unsqueeze(-1)
+        ).squeeze(-1)
+
+        loss = (advantages - self.beta * (action_logprobs - old_action_logprobs)) ** 2
+        loss = torch.mean(loss)
+        self.backward(loss)
+
+        Outputs = collections.namedtuple('Outputs', ['loss'])
+        return Outputs(loss=loss.item())
+
+
+def train_kimi(
+        policy_model_type: str,
+        policy_config_file: str,
+        max_seq_len: int,
+        lora_rank: int,
+        dtype: str,
+        lora_dtype: str,
+        lr: float,
+        epoch: int,
+        policy_ckpt_dir: str,
+        save_dir: str,
+        rollout_buffer: RolloutBuffer,
+        max_batch_size: int,
+        inner_epochs: int,
+        beta: float = None,
+        save_optim: bool = False,
+        accumulation_steps: int = 1,
+        max_num_ckpts: int = None
+):
+    policy = AutoModelForCausalLM.from_pretrained(
+        model_type=policy_model_type,
+        config_file=policy_config_file,
+        max_seq_len=max_seq_len,
+        lora_rank=lora_rank,
+        dtype=dtype,
+        lora_dtype=lora_dtype
+    )
+    optimizer = torch.optim.Adam(policy.parameters(), lr=lr)
+    trainer = ParallelKimiTrainerForCausalLM(
+        policy=policy,
+        optimizer=optimizer,
+        beta=beta,
+        save_optim=save_optim,
+        accumulation_steps=accumulation_steps
+    )
+    trainer.load_model(policy_ckpt_dir) if (
+            epoch == 0
+    ) else trainer.load(os.path.join(save_dir, "epoch-%03d" % epoch))
+    print('Policy training ...')
+    timer = Timer(total=(rollout_buffer.size() // max_batch_size) * inner_epochs, episode=100)
+    for inner_epoch in range(inner_epochs):
+        for data in rollout_buffer.get(max_batch_size, shuffle=True, output_tensor=True):
+            timer.step()
+            trainer_outputs = trainer.forward(data)
+            if trainer.step % 100 == 0:
+                print(f'--------- STEP {trainer.step} OF {timer.total} ---------')
+                print(f'Loss: {trainer_outputs.loss}')
+    trainer.save(os.path.join(save_dir, "epoch-%03d" % (epoch + 1)))
+    if max_num_ckpts is not None and (epoch + 1 - max_num_ckpts) > 0:
+        rm_dir = os.path.join(save_dir, "epoch-%03d" % (epoch + 1 - max_num_ckpts))
+        if get_rank() == 0 and os.path.exists(rm_dir):
+            shutil.rmtree(rm_dir)
+
+    policy.cpu()
+    del policy
+    del optimizer
+    del trainer
+    torch.cuda.empty_cache()
+    gc.collect()
+    set_barrier()
+
+
+def run(
+        task: str,
+        train_file: str,
+        log_dir: str,
+        save_dir: str,
+        policy_ckpt_dir: str,
+        policy_model_type: str,
+        label_file: str = None,
+        policy_config_file: str = None,
+        policy_tokenizer_file: str = None,
+        lora_rank: int = -1,
+        lora_dtype: str = "bfloat16",
+        max_batch_size: int = 1,
+        max_generate_batch_size: int = 48,
+        max_seq_len: int = 1024,
+        temperature: float = 1.0,
+        top_p: float = 1.0,
+        num_samples_per_prompt: int = 4,
+        epochs: int = 1,
+        chunk_size: int = 3072,
+        inner_epochs: int = 1,
+        lr: float = 1e-6,
+        dtype: str = "bfloat16",
+        begin_epoch: int = 0,
+        use_chat_template: bool = True,
+        seed: int = None,
+        save_optim: bool = False,
+        accumulation_steps: int = 1,
+        model_parallel_size: int = None,
+        sequence_parallel_size: int = 1
+):
+    setup_model_parallel(
+        seed=seed,
+        log_dir=log_dir,
+        log_mode="w" if begin_epoch == 0 else "a",
+        model_parallel_size=model_parallel_size,
+        sequence_parallel_size=sequence_parallel_size
+    )
+    print_current_func_args()
+    policy_config_file = policy_config_file or policy_ckpt_dir
+    policy_tokenizer_file = policy_tokenizer_file or policy_ckpt_dir
+
+    for epoch, datalist in IterationHandler(json_load(train_file), epochs, chunk_size, begin_epoch):
+        dataset = JsonDataset(datalist)
+        if len(dataset) == 0:
+            continue
+
+        # Collecting policy buffer
+        policy_rollout_buffer = collect_actor_buffer_with_label(
+            actor_model_type=policy_model_type,
+            actor_config_file=policy_config_file,
+            max_seq_len=max_seq_len,
+            actor_tokenizer_file=policy_tokenizer_file,
+            dtype=dtype,
+            actor_ckpt_dir=policy_ckpt_dir,
+            epoch=epoch,
+            actor_save_dir=save_dir,
+            use_chat_template=use_chat_template,
+            dataset=dataset,
+            max_generate_batch_size=max_generate_batch_size,
+            temperature=temperature,
+            top_p=top_p,
+            num_samples_per_prompt=num_samples_per_prompt
+        )
+
+        # Collecting verifier buffer
+        verifier_rollout_buffer = collect_rule_based_verifier_buffer(
+            actor_rollout_buffer=policy_rollout_buffer, task=task
+        )
+
+        print(f"Average Rewards: {verifier_rollout_buffer.mean()}")
+
+        # Compute GRPO rewards
+        policy_rollout_buffer, verifier_rollout_buffer = compute_grpo_rewards(
+            policy_rollout_buffer=policy_rollout_buffer,
+            verifier_rollout_buffer=verifier_rollout_buffer,
+            num_samples_per_prompt=num_samples_per_prompt
+        )
+
+        rollout_buffer = RolloutBuffer(
+            obs=policy_rollout_buffer["obs"],
+            actions=policy_rollout_buffer["actions"],
+            advantages=verifier_rollout_buffer["scores"],
+            action_masks=policy_rollout_buffer["action_masks"],
+            action_logprobs=policy_rollout_buffer["action_logprobs"],
+        )
+
+        train_kimi(
+            rollout_buffer=rollout_buffer,
+            policy_ckpt_dir=policy_ckpt_dir,
+            policy_model_type=policy_model_type,
+            policy_config_file=policy_config_file,
+            max_seq_len=max_seq_len,
+            lora_rank=lora_rank,
+            dtype=dtype,
+            lora_dtype=lora_dtype,
+            lr=lr,
+            epoch=epoch,
+            inner_epochs=inner_epochs,
+            save_dir=save_dir,
+            max_batch_size=max_batch_size,
+            save_optim=save_optim,
+            accumulation_steps=accumulation_steps
+        )
+
+        if label_file is not None:
+            evaluate_actor(
+                task=task,
+                label_file=label_file,
+                log_dir=log_dir,
+                actor_model_type=policy_model_type,
+                actor_config_file=policy_config_file,
+                max_seq_len=max_seq_len,
+                actor_tokenizer_file=policy_tokenizer_file,
+                dtype=dtype,
+                epoch=epoch,
+                actor_save_dir=save_dir,
+                max_generate_batch_size=max_generate_batch_size,
+                use_chat_template=use_chat_template
+            )
+
+
+if __name__ == '__main__':
+    fire.Fire(run)
